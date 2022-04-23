99d18156e9a78a1dd3699aadae53383d246f0b3b
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2022-04-23 21:13:37 +0300

    finish

--- task-2.js
+++ task-2.js
@@ -11,21 +11,23 @@ const users = [
   { name: 'Ajax', active: true },
   { name: 'Lux', active: false },
 ];
-const toggleUserState = (allUsers, userName, callback) => {
+
+const toggleUserState = (allUsers, userName) => {
     const updatedUsers = allUsers.map(user =>
       user.name === userName ? { ...user, active: !user.active } : user,
     );
-  
-    callback(updatedUsers);
-  };
+    return new Promise ((resolve,reject)=>{
+      resolve(updatedUsers);
+    })
+    }; 
   
   const logger = updatedUsers => console.table(updatedUsers);
   
   /*
    * Сейчас работает так
    */
-  toggleUserState(users, 'Mango', logger);
-  toggleUserState(users, 'Lux', logger);
+  // toggleUserState(users, 'Mango', logger);
+  // toggleUserState(users, 'Lux', logger);
   
   /*
    * Должно работать так
